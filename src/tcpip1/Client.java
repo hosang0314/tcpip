f553baaf3799dc96f3999042f858aff1cab05543
student <student@M11038>
2018-01-30 14:04:21 +0900

    .

--- src/tcpip1/Client.java
+++ src/tcpip1/Client.java
@@ -8,6 +8,7 @@ import java.net.UnknownHostException;
 
 public class Client {
 	public static void main(String[] args) {
+		
 		String ip = "70.12.111.130";
 		int port = 7777;
 		Socket socket = null;
