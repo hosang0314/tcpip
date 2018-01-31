dc473f745614f042d411dff65a57bcf1d5cf1b66
student <student@M11038>
2018-01-31 15:20:15 +0900

    ...

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,6 +2,10 @@ package client;
 
 public class Client {
 	public static void main(String[] args) {
-		
+		String ip = "70.12.111.139";
+		int port = 8888;
+		ClientChat chat = null;
+		chat = new ClientChat(ip, port);
+		//chat.start();
 	}
 }
